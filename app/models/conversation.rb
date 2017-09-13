33f98a6c8bb0271c3be77121d9daaf2fb65cc3fc
subbaparitala <subbaparitala@gmail.com>
2017-09-13 13:49:12 -0500

    Add database associations to models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+  validates :author, uniqueness: {scope: :receiver}
+  has_many :personal_messages, -> {order(created_at: :asc) }, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
+  has_many :personal_messages, dependent: :destroy
 end
