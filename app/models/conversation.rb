515f5ded312648966cef3fc3f9276194a885999e
subbaparitala <subbaparitala@gmail.com>
2017-09-13 16:40:28 -0500

    Add views and controller logic to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,12 @@ class Conversation < ApplicationRecord
   belongs_to :receiver, class_name: 'User'
   validates :author, uniqueness: {scope: :receiver}
   has_many :personal_messages, -> {order(created_at: :asc) }, dependent: :destroy
+
+  scope :participating, -> (user) do 
+    where("(conversations.author_id = ? OR conversations.receiver_id = ?)", user.id, user.id)
+  end
+
+  def with(current_user)
+    author == current_user ? receiver : author
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
   has_many :personal_messages, dependent: :destroy
+
+  def name
+    email.split('@')[0]
+  end
 end
